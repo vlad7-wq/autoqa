1a77a49cd89e85500eed24f827a964f5c4a79c1a
Vlad <vyuzkevych@gmail.com>
2022-12-28 18:36:36 +0200

    first commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+    WebDriver driver;
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://google.com");
+    }
+    @org.junit.Test
+    public void test() {
+        String title = driver.getTitle();
+        boolean testResult = title.contains("Google");
+        Assert.assertTrue(testResult);
+    }
+}
